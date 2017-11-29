ad2772a8b83442128e72c41679c5669682ca71cd
Chris Mansfield <chrismansfield3@hotmail.com>
2017-11-28 22:18:45 -0700

    removed web url

--- src/main/java/io/github/cmansfield/io/web/DeckCrawler.java
+++ src/main/java/io/github/cmansfield/io/web/DeckCrawler.java
@@ -7,7 +7,6 @@ import java.util.List;
 public final class DeckCrawler {
   // First string insert is the format, example: edh
   // Second string insert is the commander to search on
-  private final String DECK_SEARCH_URL = "http://tappedout.net/mtg-decks/search/?q=&format=%s&cards=&general=%s&is_top=on&price_0=&price_1=&o=-rating&submit=Filter+results";
 
   private DeckCrawler() {}
 
