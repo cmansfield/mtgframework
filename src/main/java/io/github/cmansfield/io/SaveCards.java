9d60b49621bfdd06b278ffbcc50342aacef365ab
Chris Mansfield <chrismansfield3@hotmail.com>
2017-12-01 13:44:37 -0700

    Moved the save deck logic to its own class

--- src/main/java/io/github/cmansfield/io/SaveCards.java
+++ src/main/java/io/github/cmansfield/io/SaveCards.java
@@ -47,83 +47,11 @@ public final class SaveCards {
   }
 
   /**
-   * Saves a Deck object as a json file
-   *
-   * @param deck  - The Deck object to be saved as a json file
-   * @return      - Returns a string with the filepath and filename
-   * @throws IOException
-   */
-  public static String saveDeck(Deck deck) throws IOException {
-    final String DECK_SAVE_NAME = "Deck%d.json";
-    ObjectMapper mapper = new ObjectMapper();
-    File saveFolder = createSaveDir();
-
-    String saveFileName = IoConstants.SAVE_DIR + "/" + String.format(DECK_SAVE_NAME, saveFolder.listFiles().length);
-
-    try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(saveFileName), StandardCharsets.UTF_8);) {
-
-      Map<String, Object> cardMap = new HashMap<>();
-
-      deck.getCards().forEach(card -> {
-        for(int i = 0; i < deck.getQuantity(card.getName()); ++i) {
-          cardMap.put(String.format("%s%d", card.getName(), i), card.getCardPojo());
-        }
-      });
-
-      Map<String, Object> deckMap = new HashMap<>();
-      if(deck.getFeaturedCard() != null) {
-        deckMap.put(IoConstants.FEATURED_KEY, deck.getFeaturedCard().getCardPojo());
-      }
-      if(deck.getFormat() != null) {
-        deckMap.put(Legality.FORMAT.toString(), deck.getFormat().toString());
-      }
-      deckMap.put(IoConstants.CARDS_KEY, cardMap);
-
-      String jsonCards = mapper.writerWithDefaultPrettyPrinter()
-              .writeValueAsString(deckMap);
-
-      writer.write(jsonCards);
-    }
-    catch (Exception e) {
-      System.out.printf("Unable to save the deck at this time%n%s%n", e.getMessage());
-      throw new IOException(e);
-    }
-
-    return saveFileName;
-  }
-
-  /**
-   * Saves the deck list in a 'raw' text format with only the card name and quantity
-   * This format can be used in places like http://tappedout.net
-   *
-   * @param deck  - Deck to save as a simple string format
-   * @return      - The filename of the newly saved file
-   * @throws IOException
-   */
-  public static String saveDeckRaw(Deck deck) throws IOException {
-    final String DECK_SAVE_NAME = "Deck%d.txt";
-    ObjectMapper mapper = new ObjectMapper();
-    File saveFolder = createSaveDir();
-
-    String saveFileName = IoConstants.SAVE_DIR + "/" + String.format(DECK_SAVE_NAME, saveFolder.listFiles().length);
-
-    try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(saveFileName), StandardCharsets.UTF_8);) {
-      writer.write(deck.toString());
-    }
-    catch (Exception e) {
-      System.out.printf("Unable to save the deck at this time%n%s%n", e.getMessage());
-      throw new IOException(e);
-    }
-
-    return saveFileName;
-  }
-
-  /**
    * Creates the predetermined save directory
    *
    * @return - Returns a File object of the newly created directory
    */
-  private static File createSaveDir() {
+  static File createSaveDir() {
     File saveFolder = new File(IoConstants.SAVE_DIR);
 
     try {
---
+++ src/main/java/io/github/cmansfield/io/SaveDeck.java
@@ -0,0 +1,88 @@
+package io.github.cmansfield.io;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.cmansfield.deck.Deck;
+import io.github.cmansfield.deck.constants.Legality;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class SaveDeck {
+
+  /**
+   * Saves a Deck object as a json file
+   *
+   * @param deck  - The Deck object to be saved as a json file
+   * @return      - Returns a string with the filepath and filename
+   * @throws IOException
+   */
+  public static String saveDeck(Deck deck) throws IOException {
+    final String DECK_SAVE_NAME = "Deck%d.json";
+    ObjectMapper mapper = new ObjectMapper();
+    File saveFolder = SaveCards.createSaveDir();
+
+    String saveFileName = IoConstants.SAVE_DIR + "/" + String.format(DECK_SAVE_NAME, saveFolder.listFiles().length);
+
+    try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(saveFileName), StandardCharsets.UTF_8);) {
+
+      Map<String, Object> cardMap = new HashMap<>();
+
+      deck.getCards().forEach(card -> {
+        for(int i = 0; i < deck.getQuantity(card.getName()); ++i) {
+          cardMap.put(String.format("%s%d", card.getName(), i), card.getCardPojo());
+        }
+      });
+
+      Map<String, Object> deckMap = new HashMap<>();
+      if(deck.getFeaturedCard() != null) {
+        deckMap.put(IoConstants.FEATURED_KEY, deck.getFeaturedCard().getCardPojo());
+      }
+      if(deck.getFormat() != null) {
+        deckMap.put(Legality.FORMAT.toString(), deck.getFormat().toString());
+      }
+      deckMap.put(IoConstants.CARDS_KEY, cardMap);
+
+      String jsonCards = mapper.writerWithDefaultPrettyPrinter()
+              .writeValueAsString(deckMap);
+
+      writer.write(jsonCards);
+    }
+    catch (Exception e) {
+      System.out.printf("Unable to save the deck at this time%n%s%n", e.getMessage());
+      throw new IOException(e);
+    }
+
+    return saveFileName;
+  }
+
+  /**
+   * Saves the deck list in a 'raw' text format with only the card name and quantity
+   * This format can be used in places like http://tappedout.net
+   *
+   * @param deck  - Deck to save as a simple string format
+   * @return      - The filename of the newly saved file
+   * @throws IOException
+   */
+  public static String saveDeckRaw(Deck deck) throws IOException {
+    final String DECK_SAVE_NAME = "Deck%d.txt";
+    File saveFolder = SaveCards.createSaveDir();
+
+    String saveFileName = IoConstants.SAVE_DIR + "/" + String.format(DECK_SAVE_NAME, saveFolder.listFiles().length);
+
+    try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(saveFileName), StandardCharsets.UTF_8);) {
+      writer.write(deck.toString());
+    }
+    catch (Exception e) {
+      System.out.printf("Unable to save the deck at this time%n%s%n", e.getMessage());
+      throw new IOException(e);
+    }
+
+    return saveFileName;
+  }
+}
